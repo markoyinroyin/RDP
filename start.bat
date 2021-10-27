64e7a167839f6da61ca09ed3b8c42810db555910
freljordfrost <80456172+freljordfrost@users.noreply.github.com>
2021-10-27 16:43:48 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,8 @@
 @echo off
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By Oshekher" > out.txt 2>&1
+net config server /srvcomment: "Windows Server 2019" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @Oshekher /add >nul
+net user administrator P@ssw0rd /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -12,10 +11,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+echo VPS đã tạo thành công! Hiệu lực trong 6 giờ. Hãy khởi chạy lại để tạo VPS mới. Thông tin kết nối bên dưới:
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Không thể kết nối với Ngrok!"
 echo Username: administrator
-echo Password: @Oshekher
-echo Silahkan Login Ke RDP Anda!!
+echo Password: P@ssw0rd
 ping -n 10 127.0.0.1 >nul
