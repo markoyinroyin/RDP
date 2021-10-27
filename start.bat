ece2abdb98bf5d9ede29c20a7f02028ea9dd9694
freljordfrost <80456172+freljordfrost@users.noreply.github.com>
2021-10-28 06:09:35 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,7 +13,7 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo VPS creates successfully! Alive in 6 hours, rebuild if you want to reuse it. Connection info below:
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't connecting to Ngrok"
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't connecting to Ngrok!"
 echo Username: administrator
 echo Password: P@ssw0rd
 ping -n 10 127.0.0.1 >nul
