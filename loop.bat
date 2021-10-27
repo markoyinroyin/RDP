4ff203e56c95e8114506a04b94f18307017aefe1
freljordfrost <80456172+freljordfrost@users.noreply.github.com>
2021-10-27 18:16:28 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,8 +1,8 @@
 @echo off
-echo VPS đang chạy...
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Không thể kết nối với Ngrok!" & ping 127.0.0.1 >Nul & exit
+echo VPS running
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Can't connecting to Ngrok" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > null
 cls
-echo VPS đang chạy...
+echo VPS running
 goto check
