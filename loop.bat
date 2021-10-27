cbc4d55680eb210352aace8405fec9ebbeb049c6
freljordfrost <80456172+freljordfrost@users.noreply.github.com>
2021-10-28 06:08:37 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,6 +1,6 @@
 @echo off
 echo VPS running
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Can't connecting to Ngrok" & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Can't connecting to Ngrok!" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > null
 cls
