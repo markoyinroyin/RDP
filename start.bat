a2b82e0a4d8e49d724b29f4a86d56078c4743fec
freljordfrost <80456172+freljordfrost@users.noreply.github.com>
2021-10-27 18:21:51 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -11,9 +11,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo VPS đã tạo thành công! Hiệu lực trong 6 giờ. Hãy khởi chạy lại để tạo VPS mới. Thông tin kết nối bên dưới:
+echo VPS creates successfully! Alive in 6 hours, rebuild if you want to reuse it. Connection info below:
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Không thể kết nối với Ngrok!"
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't connecting to Ngrok"
 echo Username: administrator
 echo Password: P@ssw0rd
 ping -n 10 127.0.0.1 >nul
